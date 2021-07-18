912aa1c20c115818b5b86faad220776abef656be
ibracadabra-java <rameno1995@gmail.com>
2021-07-18 00:56:55 -0400

    first commit

---
+++ wwwroot/js/AudienceSource.js
@@ -0,0 +1,26 @@
+﻿$("#industrylist").change(function () {
+    var industry = $("#industrylist").val();
+    var url = "GetSubIndustrias";
+
+    $.getJSON(url, { industria: industry }, function (data) {
+        var item = "";
+        $("#subindustry").empty();
+        $.each(data, function (i, subindustria) {
+            item += '<option value="' + subindustria.value + '">' + subindustria.text + '</option>'
+        });
+        $("#subindustry").html(item);
+    });
+});
+$("#ExtraerDatos").click(function () {
+    var industry = $("#industrylist").val();
+    var url = "/api/AudienceSource/GetSubIndustrias";
+
+    $.getJSON(url, { industria: industry }, function (data) {
+        var item = "";
+        $("#subindustry").empty();
+        $.each(data, function (i, subindustria) {
+            item += '<option value="' + subindustria.value + '">' + subindustria.text + '</option>'
+        });
+        $("#subindustry").html(item);
+    });
+});
---
+++ wwwroot/js/Stripe.js
@@ -0,0 +1,18 @@
+﻿var stripe = Stripe("pk_test_wXIajECkCDRlx9m2kMCzm7Ir00V8TMeEg5");
+var checkoutButton = document.getElementById("checkout-button");
+checkoutButton.addEventListener("click", function () {
+    var data = {
+        paymentstype: document.getElementById("payment").value,
+        Amount: document.getElementById("amount").value,
+        Currency: document.getElementById("currency").value,
+        ProductName: document.getElementById("prodname").value,
+        Mode: document.getElementById("mode").value
+    };
+    var url = "/CreateSession";
+
+    $.post(url, { data }, function (data) {
+
+        return stripe.redirectToCheckout({ sessionId: data.id });
+    });    
+      
+});
